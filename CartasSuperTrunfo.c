eb6383bd697bb9b7a3c423caa664e015cbf8b9f3
Glauco de Souza Pereira <125097992+gpereira86@users.noreply.github.com>
2025-01-29 14:28:23 +0000

    Intermediário

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,20 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
+#define ESTADOS 8
+#define CIDADES 4
+
+typedef struct {
+    char cod_cidade[4];
+    char nome_cidade[57];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+} Cidade;
+
+const char ESTADOS_LETRAS[ESTADOS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
@@ -19,50 +33,49 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
 
-    char cod_cidade[4];
-    char nome_cidade[57]; // considerado maior nome de cidade do mundo com 58 letras para determinar tamanho do char
-    
-    int populacao;
-    float area;
-    float pib;
-    int pontos_turisticos;
+    Cidade matriz[ESTADOS][CIDADES];
+    float densidadePopulacional;
+    float pibPerCapita;
 
-    
-    printf("Cadastro de Cartas\n");
-    printf("__________________\n\n");
+    printf("Digite os dados das cidades:\n");
+    for (int i = 0; i < ESTADOS; i++) {
+        for (int j = 0; j < CIDADES; j++) {
+            snprintf(matriz[i][j].cod_cidade, sizeof(matriz[i][j].cod_cidade), "%c%02d", ESTADOS_LETRAS[i], j + 1);
+            
+            printf("\nCidade [%s]:\n", matriz[i][j].cod_cidade);
+            printf("Nome da cidade: ");
+            getchar(); // Limpa buffer para fgets
+            fgets(matriz[i][j].nome_cidade, sizeof(matriz[i][j].nome_cidade), stdin);
+            printf("População: ");
+            scanf("%d", &matriz[i][j].populacao);
+            printf("Área (km²): ");
+            scanf("%f", &matriz[i][j].area);
+            printf("PIB (em bilhões): ");
+            scanf("%f", &matriz[i][j].pib);
+            printf("Número de pontos turísticos: ");
+            scanf("%d", &matriz[i][j].pontos_turisticos);
+        }
+    }
 
-    printf("Digite o código da Cidade:\n");
-    scanf(" %s", cod_cidade);
-    
-    printf("Digite o nome da Cidade:\n");
-    scanf(" %s", nome_cidade);
+    printf("\nDados das cidades:\n");
+    for (int i = 0; i < ESTADOS; i++) {
+        printf("\n=== Estado %c ===\n", ESTADOS_LETRAS[i]);
+        for (int j = 0; j < CIDADES; j++) {
+            printf("\nCidade [%s]:\n", matriz[i][j].cod_cidade);
+            printf("Nome: %s", matriz[i][j].nome_cidade);
+            printf("População: %d\n", matriz[i][j].populacao);
+            printf("Área: %.2f km²\n", matriz[i][j].area);
+            printf("PIB: %.2f bilhões\n", matriz[i][j].pib);
+            printf("Pontos turísticos: %d\n", matriz[i][j].pontos_turisticos);
+            
+            densidadePopulacional = (float)matriz[i][j].populacao /  matriz[i][j].area;
+            pibPerCapita = matriz[i][j].pib / (float)matriz[i][j].populacao;
 
-    printf("Digite a população da Cidade:\n");
-    scanf(" %d", &populacao);
-
-    while (getchar() != '\n');
-
-    printf("Digite a área da Cidade:\n");
-    scanf(" %f", &area);
-
-    while (getchar() != '\n');
-
-    printf("Digite o PIB da Cidade:\n");
-    scanf(" %f", &pib);
-
-    printf("Digite a quantidade de pontos turísticos da Cidade:\n");
-    scanf(" %d", &pontos_turisticos);
-    
-
-    printf("\n------------------------------------\n");
-    printf("Código da carta: %s;\n", cod_cidade);
-    printf("Cidade: %s;\n", nome_cidade);
-    printf("População: %d;\n", populacao);
-    printf("Cidade: %f;\n", area);
-    printf("PIB: %f;\n", pib);
-    printf("Número de pontos turísticos: %d;\n", pontos_turisticos);
-    printf("------------------------------------\n");
-
+            printf("Densidade Populacional: %.2f pessoas por km²\n", densidadePopulacional);
+            printf("PIB per Capita: %.2f bilhão por pessoa\n", pibPerCapita);
+            printf("------------------------------\n");
+        }
+    }
 
     return 0;
 }
